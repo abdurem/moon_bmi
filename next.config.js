d300e07770ea498032343d939ecd0b5d88bf7d60
abdurem <abdurehamanmustefa@gmail.com>
2025-02-02 19:44:03 +0300

    moon bmi calculator MVP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/moon_bmi',
-  assetPrefix: '/moon_bmi/',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
